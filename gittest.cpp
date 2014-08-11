8de863c67307c727b60b10be525ae1489c997c3e
eric <eric@eric-M14xR2>
2014-08-12 11:43:48 +1200

    Testing if eGit is working again!

---
+++ gittest.cpp
@@ -0,0 +1,18 @@
+/*
+ * gittest.cpp
+ *
+ *  Created on: 12/08/2014
+ *      Author: eric
+ */
+
+#include "gittest.h"
+
+git_test::git_test() {
+	// TODO Auto-generated constructor stub
+
+}
+
+git_test::~git_test() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ gittest.h
@@ -0,0 +1,18 @@
+/*
+ * gittest.h
+ *
+ *  Created on: 12/08/2014
+ *      Author: eric
+ */
+
+#ifndef GITTEST_H_
+#define GITTEST_H_
+
+class git_test {
+public:
+	git_test();
+	virtual ~git_test();
+
+	cout << "Git better be working"};
+
+#endif /* GITTEST_H_ */
