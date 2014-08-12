0452ca6da205a5236d93712a1848a5104e4030c9
shivarni <shivvy1@hotmail.com>
2014-08-13 11:59:54 +1200

    Created ROS workspace with files to use from terminal

---
+++ build/gtest/CMakeFiles/gtest_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gtest_main.dir/src/gtest_main.cc.o"
+  "/home/shivarni/indigo_workspace/project1/lib/libgtest_main.pdb"
+  "/home/shivarni/indigo_workspace/project1/lib/libgtest_main.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gtest_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
