5cc1ac051f53f938c4fb71eed9b503f482634d2a
Brian Cham <bcha899@aucklanduni.ac.nz>
2014-08-14 15:59:05 +1200

    testing the px and py for navigation

--- src/visitor.cpp
+++ src/visitor.cpp
@@ -24,8 +24,8 @@ void StageOdom_callback(nav_msgs::Odometry msg)
 	//This is the call back function to process odometry messages coming from Stage. 	
 	px = 5 + msg.pose.pose.position.x;
 	py =10 + msg.pose.pose.position.y;
-	ROS_INFO("Current x position is: %f", px);
-	ROS_INFO("Current y position is: %f", py);
+	//ROS_INFO("Current x position is: %f", px);
+	//ROS_INFO("Current y position is: %f", py);
 }
 
 
