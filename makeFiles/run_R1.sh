61d8cceefb2846cdc9347842c750372ce45c87ca
shivarni <shivvy1@hotmail.com>
2014-08-20 09:54:50 +1200

    Rione and Shivarni created an executable to run each node in a new terminal for debugging

---
+++ makeFiles/run_R1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source ../../setup.bash
+
+printf "\nCooking Robot\n"
+rosrun project1 R1
+exit
---
+++ makeFiles/run_R2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source ../../setup.bash
+
+printf "\nVisitor\n"
+rosrun project1 R2
+exit
