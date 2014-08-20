d5aa0efe028f0dc46417b189c766df5dbee25877
vedw008 <vedw008@aucklanduni.ac.nz>
2014-08-20 12:10:17 +1200

    added collisions.cpp file

---
+++ src/collisions.cpp
@@ -0,0 +1,43 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/LaserScan.h>
+#include <tf/transform_broadcaster.h>
+
+#include <sstream>
+#include "math.h"
+
+bool isClear (){
+
+}
+
+// bool isClear(    ?   all the robots   ?      )
+
+// isClear
+// method 1 = check the path is clear for every robot
+// get location and dir of robot -- GRID
+// return boolean
+
+// if true keep moving otherwise -> go to method 2
+
+// method 2 = checks which robot is bigger. Bigger robot gets first priority and moves before the small robot
+
+void detectCollision (int Robot1, int Robot2){
+
+// Check robot number 
+if (Robot1 > Robot2)
+{
+	//NAVIGATION
+
+} else
+
+{
+// Robot1 moves 
+
+}
+	//NAVIGATION
+
+// Robot2 moves 
+
+}
