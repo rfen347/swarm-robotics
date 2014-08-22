09d5c4c8e23d8db3d65c1b524a2bfcf7ed8be038
Rione-Naidoo <rione.naidoo@gmail.com>
2014-08-22 00:32:29 -0700

    Created a nurse node as r10 with dummy functions

--- se306_runnable.sh
+++ se306_runnable.sh
@@ -8,6 +8,7 @@ killall R1
 killall R2
 killall R8
 killall R9
+killall R10
 killall stageros
 killall roscore
 
@@ -24,6 +25,7 @@ rosrun project1 R1 &
 rosrun project1 R2 &
 rosrun project1 R8 &
 rosrun project1 R9 &
+rosrun project1 R10 &
 
 read -p "Press any key to exit... " -n1 -s
 killall R0
@@ -31,6 +33,7 @@ killall R1
 killall R2
 killall R8
 killall R9
+killall R10
 killall stageros
 killall roscore
 
