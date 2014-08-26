a0ac9507fe7f7c15f12847f62b12330a4a65b543
shivarni <shivvy1@hotmail.com>
2014-08-26 12:26:42 +1200

    uncommented robots in bash file

--- se306_runnable.sh
+++ se306_runnable.sh
@@ -25,17 +25,17 @@ roscd
 
 rosrun stage_ros stageros project1/world/myworld.world &
 sleep 5
-#rosrun project1 R0 &
-#rosrun project1 R1 &
-#rosrun project1 R2 &
-#rosrun project1 R3 &
-#rosrun project1 R4 &
-#rosrun project1 R5 &
+rosrun project1 R0 &
+rosrun project1 R1 &
+rosrun project1 R2 &
+rosrun project1 R3 &
+rosrun project1 R4 &
+rosrun project1 R5 &
 rosrun project1 R6 &
-#rosrun project1 R7 &
-#rosrun project1 R8 &
-#rosrun project1 R9 &
-#rosrun project1 R10 &
+rosrun project1 R7 &
+rosrun project1 R8 &
+rosrun project1 R9 &
+rosrun project1 R10 &
 
 read -p "Press any key to exit... " -n1 -s
 killall R0
