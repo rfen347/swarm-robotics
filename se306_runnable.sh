3f7f9809c2f409e2d5622b1dfddb06fb7cec2c47
shivarni <shivvy1@hotmail.com>
2014-08-26 18:25:22 +1200

    Added spinning to doctor methods

--- se306_runnable.sh
+++ se306_runnable.sh
@@ -34,8 +34,8 @@ sleep 5
 #rosrun project1 R6 &
 #rosrun project1 R7 &
 #rosrun project1 R8 &
-rosrun project1 R9 &
-#rosrun project1 R10 &
+#rosrun project1 R9 &
+rosrun project1 R10 &
 
 read -p "Press any key to exit... " -n1 -s
 killall R0
